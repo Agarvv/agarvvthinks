76eb22afa8eeea4ebf968f7e47fdebbde6468d1d
Agarvv <162643502+Agarvv@users.noreply.github.com>
2025-06-18 23:54:41 +0000

    server started

---
+++ server/src/main.rs
@@ -0,0 +1,27 @@
+use actix_web::{get, post, web, App, HttpResponse, HttpServer, Responder};
+use dotenvy::dotenv;
+use std::env;
+use sea_orm::{Database, DatabaseConnection};
+
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    dotenv().ok();
+    let database_url = env::var("DATABASE_URL").expect("kein DATABASE_URL");
+    let db = Database::connect(&database_url).await.expect("Fehler");
+
+    HttpServer::new(move || {
+        App::new()
+            .app_data(web::Data::new(db.clone()))
+            .service(hello)
+    })
+    .bind(("127.0.0.1", 8080))?
+    .run()
+    .await
+}
+
+#[get("/")]
+async fn hello() -> impl Responder {
+    HttpResponse::Ok().body("Hello world!")
+}
+
